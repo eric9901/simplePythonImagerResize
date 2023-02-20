664b46ea6e5af7b3b35cad45a789521cd31694ad
ekmh <erickwok9901@gmail.com>
2023-02-20 12:13:23 +0800

    --the program run required user input

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import sys
 import resizer
+from os import listdir
+from os.path import isfile, join,isdir
 if __name__ == '__main__':
-    resizer.resize('tg',"sea2.jpg")
+    path=sys.argv[1]
+    try:
+        if isdir(path):
+            for f in listdir(path):
+                if isfile(join(path, f)):
+                    resizer.resize('tg',join(path, f))
+        elif isfile(path):
+            resizer.resize('tg',path)
+        else:
+            print("please input correct folder or file path")
+    except IOError:
+        print("file not found")
